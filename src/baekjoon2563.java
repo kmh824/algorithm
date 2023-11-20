b9f6fe1309313e76feb127004c3e07097bd6e5a7
kmh824 <1rkdalsgur1@naver.com>
2023-11-20 23:54:24 +0900

    commit message

--- src/baekjoon2563.java
+++ src/baekjoon2563.java
@@ -1,5 +1,7 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 public class baekjoon2563 {
     public static void main(String[] args) {
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
