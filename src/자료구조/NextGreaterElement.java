5b43f24816c246da284219b7858134d43769f13f
MinHyuk Kang <1rkdalsgur1@naver.com>
2024-06-11 02:26:48 +0900

    NextGreaterElement

--- src/자료구조/NextGreaterElement.java
+++ src/자료구조/NextGreaterElement.java
@@ -8,36 +8,35 @@ import java.util.Stack;
 
 /**
  * 오큰수
+ * stack에 배열의 index를 저장
  */
 public class NextGreaterElement {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
 
         int N = Integer.parseInt(br.readLine());
-        int[] result = new int[N];
         int[] sequence = new int[N];
-        Stack<Integer> stack = new Stack<>();
 
         String[] input = br.readLine().split(" ");
         for (int i = 0; i < N; i++) {
             sequence[i] = Integer.parseInt(input[i]);
         }
 
-        for (int i = N - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && stack.peek() <= sequence[i]) {
-                stack.pop();
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && sequence[stack.peek()] < sequence[i]) {
+                sequence[stack.pop()] = sequence[i];
             }
-            if (stack.isEmpty()) {
-                result[i] = -1;
-            }
-            else if (stack.peek() > sequence[i]) {
-                result[i] = stack.peek();
-            }
-            stack.push(sequence[i]);
+            stack.push(i);
         }
+        while (!stack.isEmpty()) {
+            sequence[stack.pop()] = -1;
+        }
+        for (int i : sequence) {
+            sb.append(i).append(' ');
+        }
+        System.out.println(sb);
 
-        for (int i : result) {
-            System.out.print(i + " ");
-        }
     }
 }
