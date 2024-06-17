908f1106daf8b67491bbaca6f6badd87ad9568fe
MinHyuk Kang <1rkdalsgur1@naver.com>
2024-06-17 16:52:05 +0900

    PostfixExpressionConverter

--- src/자료구조/PostfixExpressionConverter.java
+++ src/자료구조/PostfixExpressionConverter.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
+//중위 표기식 -> 후위 표기식
 public class PostfixExpressionConverter {
 
     private static int precedence(char ch) {
