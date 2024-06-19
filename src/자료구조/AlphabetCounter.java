4c64c849d28d2797c95034ef0a5fc75e774adaed
MinHyuk Kang <1rkdalsgur1@naver.com>
2024-06-19 23:53:40 +0900

    AlphabetCounter

--- src/자료구조/AlphabetCounter.java
+++ src/자료구조/AlphabetCounter.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-
+//알파벳 수
 public class AlphabetCounter {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
